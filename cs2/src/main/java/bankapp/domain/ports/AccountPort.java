ea915399af5ec1923835b9ef22b01f31ab9396c8
Teo-SoftDev <mateo.guisao@correo.tdea.edu.co>
2026-04-01 16:20:01 -0500

    ADD - create CreateAccount service

--- cs2/src/main/java/bankapp/domain/ports/AccountPort.java
+++ cs2/src/main/java/bankapp/domain/ports/AccountPort.java
@@ -6,6 +6,8 @@ import bankapp.domain.models.Account;
 import bankapp.domain.models.AccountState;
 
 public interface AccountPort {
+
+    public boolean existsByAccountNumber(String accountNumber);
     
     public void saveAccount(Account account);
     public void updateBalance(BigDecimal balance);
--- cs2/src/main/java/bankapp/domain/services/CreateAccount.java
+++ cs2/src/main/java/bankapp/domain/services/CreateAccount.java
@@ -1,5 +1,20 @@
 package bankapp.domain.services;
 
+import bankapp.domain.Exceptions.BusinessException;
+import bankapp.domain.ports.AccountPort;
+import bankapp.domain.models.Account;
+
 public class CreateAccount {
-    
+
+    private AccountPort accountPort;
+
+    public void createAccount(Account account) throws BusinessException {
+
+        if (accountPort.existsByAccountNumber(account.getAccountNumber())) {
+            throw new BusinessException("There is already an account with that account number.");
+        }
+
+        accountPort.saveAccount(account);
+        
+    }
 }
