c7b41fcdb7813cf1882e906d809c6b68f1deab72
Teo-SoftDev <mateo.guisao@correo.tdea.edu.co>
2026-05-07 17:17:49 -0500

    ADD - start creating AdministrativeUseCase and run the db

---
+++ cs2/src/main/java/bankapp/application/usecases/AdministrativeUseCase.java
@@ -0,0 +1,10 @@
+package bankapp.application.usecases;
+
+import bankapp.domain.services.CreateBusinessClient;
+import bankapp.domain.services.CreateNaturalClient;
+
+public class AdministrativeUseCase {
+    private final CreateNaturalClient createNaturalClient;
+    private final CreateBusinessClient createBusinessClient;
+    
+}
